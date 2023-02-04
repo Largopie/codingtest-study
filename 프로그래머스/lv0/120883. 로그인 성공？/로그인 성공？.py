e5e9fb2335b8d2eda1f27147f81b5f7b22ba4216
JaeSeok <106071687+Largopie@users.noreply.github.com>
2023-02-04 15:00:56 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.py
@@ -0,0 +1,8 @@
+def solution(id_pw, db):
+    for i in range(len(db)):
+        if db[i][0] == id_pw[0]:
+            if db[i][1] == id_pw[1]:
+                return "login"
+            else:
+                return "wrong pw"
+    return "fail"
