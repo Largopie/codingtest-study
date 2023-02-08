258b81a4802d8cfb75514aca1c895eecec56afde
JaeSeok <106071687+Largopie@users.noreply.github.com>
2023-02-08 15:43:36 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,5 +1,8 @@
 def solution(array):
-    arr = [0 for _ in range(1000)]
-    for i in array:
-        arr[i]+=1
-    return -1 if arr.count(max(arr)) >= 2 else arr.index(max(arr))
+    count = {}
+    if len(array) == 1:
+        return array[0]
+    for i in set(array):
+        count[i] = array.count(i)
+    count = sorted(count.items(), key=lambda x: x[1], reverse=True)
+    return -1 if count[0][1] == count[1][1] else count[0][0]
