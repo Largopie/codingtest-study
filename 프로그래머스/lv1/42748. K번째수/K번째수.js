76e72044763a730276752940872db0c3f1bc060b
JaeSeok <106071687+Largopie@users.noreply.github.com>
2023-07-25 15:48:49 +0900

    [level 1] Title: K번째수, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -0,0 +1,10 @@
+function solution(array, commands) {
+    const answer = [];
+    
+    for(command of commands){
+        const arr = array.slice(command[0] - 1, command[1]).sort((a,b) => a - b);
+        answer.push(arr[command[2] - 1])
+        
+    }
+    return answer;
+}
