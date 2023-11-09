8e957ba0adf8190bf9cb3b3256aa58793401e164
Song Jae Seok <106071687+Largopie@users.noreply.github.com>
2023-11-09 10:00:16 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -2,7 +2,7 @@ function solution(s) {
     const splitString = s.split(' ');
     
     const JadenCase = splitString.map((str) => {
-        if(str === '') return;
+        if(str === '') return '';
         return str[0].toUpperCase() + str.slice(1).toLowerCase()
     })
     
