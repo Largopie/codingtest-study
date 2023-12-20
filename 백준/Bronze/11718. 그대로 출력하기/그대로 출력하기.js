9a249648b90f43b539cb055e1ada3781d5ec7731
Song Jae Seok <106071687+Largopie@users.noreply.github.com>
2023-12-20 20:56:08 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 148 ms, Memory: 9496 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.js
@@ -0,0 +1,19 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+  input.push(line);
+}).on('close', function () {
+  solution(input);
+  process.exit();
+});
+
+const solution = (input) => {
+  console.log(input.join('\n'));
+};
