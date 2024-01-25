545e5cc0254b4d3fbd6350738ca835afe2c0ebcf
Song Jae Seok <106071687+Largopie@users.noreply.github.com>
2024-01-25 18:32:42 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 2040 ms, Memory: 224840 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -0,0 +1,24 @@
+const readline = require('readline');
+const { Z_ASCII } = require('zlib');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+  input.push(line);
+}).on('close', function () {
+  solution(input);
+  process.exit();
+});
+
+const solution = (input) => {
+  const inputArr = input.slice(1).map((val) => +val);
+
+  inputArr.sort((a, b) => a - b);
+
+  console.log(inputArr.join('\n'));
+};
