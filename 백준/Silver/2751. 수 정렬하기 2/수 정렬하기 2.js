3732074829a0199570f7b1f005d1f10ac65810d1
Song Jae Seok <106071687+Largopie@users.noreply.github.com>
2024-01-26 14:33:05 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1440 ms, Memory: 215780 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -18,7 +18,39 @@ rl.on('line', function (line) {
 const solution = (input) => {
   const inputArr = input.slice(1).map((val) => +val);
 
-  inputArr.sort((a, b) => a - b);
+  const swap = (arr, left, right) => {
+    const temp = arr[left];
 
-  console.log(inputArr.join('\n'));
+    arr[left] = arr[right];
+    arr[right] = temp;
+  };
+
+  const partition = (arr, left, right) => {
+    const pivot = arr[left + Math.round(Math.random() * (right - left))];
+
+    while (left <= right) {
+      while (arr[left] < pivot) left++;
+      while (arr[right] > pivot) right--;
+
+      if (left <= right) {
+        swap(arr, left, right);
+        left++;
+        right--;
+      }
+    }
+    return left;
+  };
+
+  const quickSort = (arr, left, right) => {
+    if (left >= right) return arr;
+
+    let index = partition(arr, left, right);
+
+    if (left < index - 1) quickSort(arr, left, index - 1);
+    if (index < right) quickSort(arr, index, right);
+
+    return arr;
+  };
+
+  console.log(quickSort(inputArr, 0, inputArr.length - 1).join('\n'));
 };
