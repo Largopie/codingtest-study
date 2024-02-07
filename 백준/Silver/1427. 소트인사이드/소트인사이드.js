ef6c1ab322d67ab6cec7f8de538ea39db1e6b77e
Song Jae Seok <106071687+Largopie@users.noreply.github.com>
2024-02-07 22:47:12 +0900

    [Silver V] Title: 소트인사이드, Time: 140 ms, Memory: 9436 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.js
@@ -0,0 +1,21 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', function (line) {
+  solution(line);
+  rl.close();
+}).on('close', function () {
+  process.exit();
+});
+
+const solution = (input) => {
+  const inputValue = input.split('').map((val) => +val);
+
+  inputValue.sort().reverse();
+
+  console.log(inputValue.join(''));
+};
