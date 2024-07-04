fd471b157afc7130bc0cb5796d362ce16dab533a
낙타/송재석 <106071687+Largopie@users.noreply.github.com>
2024-07-04 20:53:46 +0900

    [Bronze IV] Title: DKSH 찾기, Time: 172 ms, Memory: 9448 KB -BaekjoonHub

---
+++ 백준/Bronze/29766. DKSH 찾기/DKSH 찾기.js
@@ -0,0 +1,17 @@
+const solution = (input) => {
+  return input.match(/DKSH/g)?.length ?? 0;
+};
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', function (line) {
+  console.log(solution(line));
+  rl.close();
+}).on('close', function () {
+  process.exit();
+});
