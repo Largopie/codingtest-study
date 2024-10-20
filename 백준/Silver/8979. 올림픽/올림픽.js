2c0921dda415d51a886d1a5c8bde8cbec181dfed
낙타/송재석 <106071687+Largopie@users.noreply.github.com>
2024-10-20 21:14:22 +0900

    [Silver V] Title: 올림픽, Time: 160 ms, Memory: 12264 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.js
+++ 백준/Silver/8979. 올림픽/올림픽.js
@@ -2,33 +2,27 @@ const [firstInput, ...input] = require('fs').readFileSync('/dev/stdin').toString
 
 const countryInfo = input.map((line) => line.split(' ').map(Number));
 const [N, K] = firstInput.split(' ').map(Number);
-const prizes = Array.from({ length: N }, () => 0);
 
-countryInfo.sort((a, b) => {
-  if (a[1] > b[1]) return -1;
-  else if (a[1] < b[1]) return 1;
+countryInfo.sort(([aId, aGold, aSilver, aBronze], [bId, bGold, bSilver, bBronze]) => {
+  if (aGold < bGold) return 1;
+  else if (aGold > bGold) return -1;
   else {
-    if (a[2] > b[2]) return -1;
-    else if (a[2] < b[2]) return 1;
+    if (aSilver < bSilver) return 1;
+    else if (aSilver > bSilver) return -1;
     else {
-      if (a[3] > b[3]) return -1;
-      else return 1;
+      if (aBronze < bBronze) return 1;
+      else return -1;
     }
   }
 });
 
-let p = 0;
+const ansIdx = countryInfo.findIndex((ele) => ele[0] === K);
 
-countryInfo.forEach(([countryId, ...prize], idx) => {
-  if (countryInfo[idx - 1]) {
-    const [beforeCountryId, ...beforePrize] = countryInfo[idx - 1];
+for (let i = ansIdx - 1; i >= 0; i--) {
+  if (countryInfo[ansIdx].slice(1).some((ele, idx) => ele !== countryInfo[i][idx + 1])) {
+    console.log(i + 2);
+    return;
+  }
+}
 
-    if (beforePrize.every((val, index) => val === prize[index])) {
-      prizes[countryId - 1] = p;
-    } else {
-      prizes[countryId - 1] = ++p;
-    }
-  } else prizes[countryId - 1] = ++p;
-});
-
-console.log(prizes[K - 1]);
+console.log(ansIdx + 1);
